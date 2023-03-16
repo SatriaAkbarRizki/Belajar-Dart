62ec1dfd29464f91258d5d24fe713ed64f7fd2ed
Satria_AR <sartiapl@gmail.com>
2023-03-16 11:55:59 +0700

    file baru lagi

---
+++ assrt.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  int number = -5;
+  assert(number > 5, 'number harus lebih besar dari angka 5');
+  print("Nilai diatas : $number");
+}
+
+// Assert mirip dengan try exception cuman ini lebih simple aja
